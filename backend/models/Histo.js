27af2e0787e4201ae98dfa1a2971739adefdb149
Nicolas Rozoy <atserio@live.fr>
2022-05-17 22:30:11 -0400

    modified:   backend/controllers/balances.js 	deleted:    backend/controllers/src/prices.js 	modified:   backend/models/Histo.js 	backend/controllers/Objects/ 	backend/models/LastBalance.js

--- backend/models/Histo.js
+++ backend/models/Histo.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const histoSchema = mongoose.Schema({
   day: { type: Number, required: true },
   balance: { type: Number, required: true },
+  balance_eth: { type: Number, required: true },
+  balance_btc: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("Histo", histoSchema);
